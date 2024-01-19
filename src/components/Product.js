bf0c22f2c85eabce9fd12b19164c36867b70b427
ILIAS TSELIOS <ilias@tselios.com>
2024-01-19 21:18:34 +0200

    initial commit

---
+++ src/components/Product.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Product(props) {
+  const { product, onAdd } = props;
+  return (
+    <div>
+      <img className="small" src={product.thumbnail} alt={product.title}></img>
+      <h3>{product.title}</h3>
+      <div>${product.price}</div>
+      <div>
+        <button onClick={() => onAdd(product)}>Add To Cart</button>
+      </div>
+    </div>
+  );
+}
